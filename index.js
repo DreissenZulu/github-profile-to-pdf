fd3a8d04a35298666ffa2ab079cdd721b2ac0d96
Alex Khoa Le <leduykhoa@hotmail.ca>
2019-10-25 12:56:06 -0400

    Writes HTML to file

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const genHTML = require("./generateHTML");
+const genHTML = require("./generateHTML.js");
 const inquirer = require("inquirer");
 const axios = require("axios");
-const PDFDoctument = require('phantom-html2pdf');
+const PDFDoctument = require('html-pdf');
 const fs = require('fs');
 
 const questions = [
@@ -24,19 +24,26 @@ const questions = [
 ];
 
 function writeToFile(fileName, data) {
-
-
+    fs.writeFile(fileName, genHTML.generateHTML(data), function(err) {
+        if (err) throw err;
+        console.log("Successfully saved!");
+    })
 }
 
 function init() {
     inquirer.prompt(questions).then(function(data) {
-        let queryURL = `https://api.github.com/users/${data.username}`;
-        console.log(queryURL);
-        axios
-        .get(queryURL).then(response => {
-            console.log(`Name: ${response.data.name} Profile Image: ${response.data.avatar_url} Location: ${response.data.location} GitHub Profile: ${response.data.html_url} Blog: ${response.data.blog} Bio: ${response.data.bio} Public Repos: ${response.data.public_repos} Followers: ${response.data.followers} `);
-        }) 
+        // let queryURL = `https://api.github.com/users/${data.username}`;
+        // console.log(queryURL);
+        // axios
+        // .get(queryURL).then(response => {
+        //     console.log(`Name: ${response.data.name} Profile Image: ${response.data.avatar_url} Location: ${response.data.location} GitHub Profile: ${response.data.html_url} Blog: ${response.data.blog} Bio: ${response.data.bio} Public Repos: ${response.data.public_repos} Followers: ${response.data.followers} `);
+        // }) 
+        writeToFile(`${data.username}.html`, data);
     });
 }
 
 init();
+
+// var thisThing = [{color: "red"}, {name: "Ayo"}];
+
+// writeToFile("index.html", thisThing);
